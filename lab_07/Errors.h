2e684a9ed21a5d56fbd8fddeffb60094c4c0dc62
LoaDeadd81 <33088339+LoaDeadd81@users.noreply.github.com>
2022-04-29 13:19:19 +0300

    added lab7

---
+++ lab_07/Errors.h
@@ -0,0 +1,29 @@
+#ifndef ERRORS_H
+#define ERRORS_H
+
+#include <exception>
+#include <QString>
+
+
+class Error : public std::exception
+{
+private:
+    QString error_message;
+public:
+    Error(QString message);
+    QString get_message();
+};
+
+class UIError : public Error
+{
+public:
+    UIError(QString message);
+};
+
+class Todo : public Error
+{
+public:
+    Todo(QString message);
+};
+
+#endif 
