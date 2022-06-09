0020aa0bbc63f5e47faddccf3b7463422c98370e
LoaDeadd81 <33088339+LoaDeadd81@users.noreply.github.com>
2022-06-09 23:41:27 +0300

    Add files via upload

---
+++ lab_02/inc/mainwindow.h
@@ -0,0 +1,61 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QPen>
+#include <QRect>
+#include <QErrorMessage>
+
+#include "Figure.h"
+#include "MyErrors.h"
+#include "Pair.h"
+
+using namespace std;
+
+typedef DiffTypePair<Figure, QString> action_type;
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_ParamButton_clicked();
+
+    void on_MoveButton_clicked();
+
+    void on_ScaleButton_clicked();
+
+    void on_RotateButton_clicked();
+
+    void on_CancelButton_clicked();
+
+    void on_SelectCenterButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    Figure figure;
+    QPen main_pen;
+    QGraphicsScene *main_scene;
+    vector <DiffTypePair<Figure, QString>> action_vec;
+    bool has_figure;
+
+    Point get_transform_center();
+    void set_default_params();
+    void create_scene();
+    void create_figure();
+
+    bool is_empty_action_list();
+    void push_action(action_type action);
+    action_type pop_action();
+    void clean_actions();
+};
+#endif
